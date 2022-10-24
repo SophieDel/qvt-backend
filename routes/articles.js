96a0e85d8d0eb8443ff0a6308a7cd79237128d4f
SophieDel <sophie.demeulemeester@gmail.com>
2022-10-24 11:49:32 +0200

    [qvt-fronteend] check themes

--- routes/articles.js
+++ routes/articles.js
@@ -1,16 +1,23 @@
 var express = require("express");
 var router = express.Router();
-const { checkBody } = require('../modules/checkBody');
+const { checkBody } = require("../modules/checkBody");
 
 const Article = require("../models/articles");
 
+const themes = ["santé", "stress", "épanouissement", "conditions de travail"];
 router.post("/new", (req, res) => {
   // Par cette route on ajoute un nouvel article dans la base de données
 
+  // On vérifie que tous les champs nécessaires à la création d'une nouvelle entrée dans la base articles soient tous rentrés
   if (
     !checkBody(req.body, ["titre", "theme", "resume", "illustration", "lien"])
   ) {
-    res.json({ result: false, error: "Missing or empty fields" });
+    res.json({ result: false, error: "Un champ de saisie manque." });
+    return;
+  }
+
+  if (!themes.includes(req.body.theme)) {
+    res.json({ result: false, error: "Le thème entré n'est pas parmi la liste de thèmes définis" });
     return;
   }
 
@@ -24,7 +31,7 @@ router.post("/new", (req, res) => {
 
   newArticle
     .save()
-    .then(data => res.json({ result: true, article: data }))
+    .then((data) => res.json({ result: true, article: data }))
     .catch((error) => req.json({ result: false, error: error }));
 });
 
