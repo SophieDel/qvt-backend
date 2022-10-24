b1b8e25f52b3ab72807b659a7b33f0caa7166a0a
SophieDel <sophie.demeulemeester@gmail.com>
2022-10-24 11:12:07 +0200

    [qvt-fronteend] add article route

--- routes/articles.js
+++ routes/articles.js
@@ -1,27 +1,31 @@
-var express = require("express")
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
+const { checkBody } = require('../modules/checkBody');
 
-const Article = require("../models/articles")
+const Article = require("../models/articles");
 
 router.post("/new", (req, res) => {
-    
-    const rawTweet = req.body.tweet;
-    // Mettre le tweet sous forme d'array
-    const contentInArray = rawTweet.split(" ");
-    // Isoler les hashtage dans le Tweet
-    const hashtagsArray = contentInArray
-      .filter((e) => e[0] == "#")
-      .map((e) => e.toLowerCase());
-    // Chaque hashtag est unique
-    const hashtags = hashtagsArray ? [...new Set(hashtagsArray)] : [];
-    const newTweet = new Tweet({
-      rawTweet: rawTweet,
-      contentInArray: contentInArray,
-      hashtags: hashtags,
-    });
-  
-  
-  
-  
-  
-  module.exports = router;
+  // Par cette route on ajoute un nouvel article dans la base de données
+
+  if (
+    !checkBody(req.body, ["titre", "theme", "resume", "illustration", "lien"])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  const newArticle = new Article({
+    titre: req.body.titre,
+    theme: req.body.theme,
+    resume: req.body.resume,
+    illustration: req.body.illustration,
+    lien: req.body.lien,
+  });
+
+  newArticle
+    .save()
+    .then(data => res.json({ result: true, article: data }))
+    .catch((error) => req.json({ result: false, error: error }));
+});
+
+module.exports = router;
