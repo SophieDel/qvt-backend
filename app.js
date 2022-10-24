8a37b19d84d0494cd069c4010e0f89859c91217b
SophieDel <sophie.demeulemeester@gmail.com>
2022-10-24 11:04:08 +0200

    [qvt-fronteend] debug app

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const cors = require('cors');
-app.use(cors());
+var articlesRouter = require('./routes/articles');
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
 
 module.exports = app;
 
