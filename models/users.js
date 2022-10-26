2ccb1bb8b2298872e5cd071f45d54605c9852217
fred <jaubertf@yahoo.fr>
2022-10-26 12:19:23 +0200

    test

--- models/users.js
+++ models/users.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+//Tableau de sous documents pour acceuillir les résultats des questionnaires hebdo
+const QHebdoSchema = mongoose.Schema ({
+  Q1 : Number,
+  Q2 : Number,
+  Q3 : Number,
+});
+
+
 const userSchema = mongoose.Schema({
   genre : String,
   nom : String,
@@ -16,7 +24,15 @@ const userSchema = mongoose.Schema({
   RGPDParternaire : Boolean,
   cgu: Boolean,
  // questionnaire: { type: mongoose.Schema.Types.ObjectId, ref: 'questionnaires' },
+  QHebdo : [QHebdoSchema],
   });
+
+
+
+
+
+
+
 
 const User = mongoose.model('users', userSchema);
 
