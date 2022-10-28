3e48b79389f3e80f3e976b82a8534d04e9e1cc19
fred <jaubertf@yahoo.fr>
2022-10-28 08:20:44 +0200

    plan action

---
+++ models/plans.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+//Tableau de sous documents pour acceuillir les Plans d'action
+const PlanSchema = mongoose.Schema ({
+    datePlan : Date,
+    message : String,
+    equipe : String,
+    encours : Boolean,
+  });
+
+  
+const Plan = mongoose.model('plans', PlanSchema);
+
+module.exports = Plan;
--- routes/messages.js
+++ routes/messages.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
 require('../models/connection');
 const Message = require('../models/messages');
+const Plan = require('../models/plans')
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 // const bcrypt = require('bcrypt');
 const bcrypt = require('bcryptjs');
-
-
 
 
 //ROUTE Envoi d'un message d'un collab
@@ -18,7 +16,6 @@ router.post('/MessageCollab/:token', (req, res) => {
   
      //la date d'enoiv
   let currentdate = new Date();
-
 
   const newMessage = new Message({
     dateRecep : currentdate,
@@ -36,7 +33,6 @@ newMessage.save().then(data => {
 
 
 //ROute récupération des messages d'une équipe par le manager
-
 router.get('/MessageEquipe/:equipe', (req, res) => {
     Message.find({ equipe: req.params.equipe , repondu : false}).then(data => {
 
@@ -46,22 +42,12 @@ router.get('/MessageEquipe/:equipe', (req, res) => {
 
   
 //ROUTE Réponse du manager
-
 router.post('/MessageReponse', (req, res) => {
-
-
-             //la date d'enoiv
-  let currentdate = new Date();
-
-
       
            Message.updateOne(
-                // { dateReponse : currentdate},
                 {message : req.body.message},
                 { reponse : req.body.reponse, repondu : true},
-                // {repondu : true},
             ).then(()=> {
-            // console.log ("data updatée" ,data);
               res.json({ result: true});
             })
           } )
@@ -77,6 +63,38 @@ router.get('/MessageCollab/:token', (req, res) => {
       });
   });
   
+
+  
+//ROUTE Envoi d'un Plan d'action par le manager
+router.post('/PlanManager/:token', (req, res) => {
+  User.findOne({ token: req.params.token }).then(data => {
+
+   //la date d'enoiv
+let currentdate = new Date();
+
+
+const newPlan = new Plan({
+  datePlan : currentdate,
+  message : req.body.message,
+  equipe : data.equipe,
+  encours : true,
+});
+newPlan.save().then(data => {
+  res.json({ result: true});
+  console.log(data)
+});
+  });
+});
+
+//ROute récupération des plans d'action par l"équipe
+router.get('/PlanEquipe/:equipe', (req, res) => {
+  Plan.find({ equipe: req.params.equipe, encours : true}).then(data => {
+
+       res.json({data});
+    });
+});
+
+
 
 
 
